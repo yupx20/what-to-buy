fc8ecbe26afaa837ab3ba15c0d699040f9f834d2
yupx20 <muhammad.rizal.yudianto-2021@fst.unair.ac.id>
2026-08-12 20:51:03 +0700

    set pgsql sslmode

--- config/database.php
+++ config/database.php
@@ -96,7 +96,8 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'require',
+            // 'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
         'sqlsrv' => [
